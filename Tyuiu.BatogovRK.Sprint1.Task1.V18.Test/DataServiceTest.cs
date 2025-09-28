287dafa15673e7c98641358c1a4fee40e2a1331c
BatogovRoman <romanbatogov@gmail.com>
2025-09-28 13:52:25 +0500

    Task1

---
+++ Tyuiu.BatogovRK.Sprint1.Task1.V18.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿
+using Tyuiu.BatogovRK.Sprint1.Task1.V18.Lib;
+
+namespace Tyuiu.BatogovRK.Sprint1.Task1.V18.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 1.0;
+            double y = 2.0;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(-2, res);
+        }
+    }
+}
