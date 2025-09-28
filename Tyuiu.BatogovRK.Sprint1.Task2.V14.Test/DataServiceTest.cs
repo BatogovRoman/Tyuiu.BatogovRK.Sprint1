4c968e185222c53e2c0f80557671f715786a02d4
BatogovRoman <romanbatogov@gmail.com>
2025-09-28 12:53:14 +0500

    Task2

---
+++ Tyuiu.BatogovRK.Sprint1.Task2.V14.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.BatogovRK.Sprint1.Task2.V14.Lib;
+
+namespace Tyuiu.BatogovRK.Sprint1.Task2.V14.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = 0;
+            var res = ds.ConvertKelvinToCelsius(x);
+            Assert.AreEqual(- 273, res);
+        }
+    }
+}
---
+++ Tyuiu.BatogovRK.Sprint1.Task2.V14/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.BatogovRK.Sprint1.Task2.V14.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #1 | Выполнил: Батогов Р. К. | АСОиУб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+        Console.WriteLine("* Задание #2                                                              *");
+        Console.WriteLine("* Вариант 14                                                              *");
+        Console.WriteLine("* Выполнил: Батогов Роман Константинович | АСОиУб-25-1                    *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+        Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("*  ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+        Console.WriteLine("***************************************************************************");
+
+        int x;
+
+        Console.WriteLine("Введите температуру в градусах Кельвина:");
+
+        x = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ                                                               *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Температура в градусах Цельсия равна " + ds.ConvertKelvinToCelsius(x));
+
+        Console.ReadLine();
+    }
+}
