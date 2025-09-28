a13c0cda47f1f0ca96cc1815317493f999537b84
BatogovRoman <romanbatogov@gmail.com>
2025-09-28 15:33:01 +0500

    Task4

---
+++ Tyuiu.BatogovRK.Sprint1.Task4.V3.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.BatogovRK.Sprint1.Task4.V3.Lib;
+
+namespace Tyuiu.BatogovRK.Sprint1.Task4.V3.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 5;
+            double y = 3.9;
+            double need = 3.201;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(need, res);
+        }
+    }
+}
---
+++ Tyuiu.BatogovRK.Sprint1.Task4.V3/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.BatogovRK.Sprint1.Task4.V3.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #1 | Выполнил: Батогов Р. К. | АСОиУб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+        Console.WriteLine("* Задание #4                                                              *");
+        Console.WriteLine("* Вариант 3                                                               *");
+        Console.WriteLine("* Выполнил: Батогов Роман Константинович | АСОиУб-25-1                    *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+        Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("*  ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+        Console.WriteLine("***************************************************************************");
+
+        double x, y;
+        Console.WriteLine("Введите значение X:");
+        x = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите значение Y:");
+        y = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ                                                               *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Sqrt(3 * x) / (x - y)^2 = " + ds.Calculate(x, y));
+        Console.ReadKey();
+    }
+}
